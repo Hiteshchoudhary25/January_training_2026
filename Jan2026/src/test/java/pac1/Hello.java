495b3d2249b96b771d51eb884780fb8971627253
Hiteshchoudhary25 <hiteshchoudhary2508@gmail.com>
2026-01-13 16:29:46 +0530

    13th Jan commits

---
+++ Jan2026/src/test/java/pac1/Hello.java
@@ -0,0 +1,17 @@
+package pac1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Persons Details");
+		System.out.println("--------");
+		System.out.println("First Name : Hitesh");
+		System.out.println("Last Name : Choudhary");
+		System.out.println("Gender : M");
+		System.out.println("Age : 22");
+		System.out.println("Weight : 70");
+
+	}
+
+}
