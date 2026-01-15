a055a9bc60d364cb6f708c3025f282e85adfb798
Hiteshchoudhary25 <hiteshchoudhary2508@gmail.com>
2026-01-15 15:09:27 +0530

    Code till 15th Jan Noon till lab 14

--- Jan2026/src/test/java/pac3/lab6.java
+++ Jan2026/src/test/java/pac3/lab6.java
@@ -4,16 +4,8 @@ public class lab6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Person p = new Person();
-//		p.setFirstName("Hitesh");
-//		p.setLastName("Choudhary");
-//		p.setGender('M');
-//		sysout
-//		System.out.println("Name : " + p.getFirstName());
-//		System.out.println("Lastname : " + p.getLastName());
-//		System.out.println("Gender is :" + p.getGender());
 		
-		Person obj = new Person("Hitesh" , "Patel" , 'M');
+		Person obj = new Person("Hitesh" , "Patel" , 'M', null);
 		System.out.println("Name : " + obj.getFirstName());
 		System.out.println("Lastname : " + obj.getLastName());
 		System.out.println("Gender is :" + obj.getGender());
